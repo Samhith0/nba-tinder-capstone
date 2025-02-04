d30dd6f2af5df97f1108b572522dbf257c368ed4
ohvogelpohl <100149455+ohvogelpohl@users.noreply.github.com>
2025-02-03 20:41:52 -0500

    basic endpoints

--- frontend-nba-tinder/src/App.tsx
+++ frontend-nba-tinder/src/App.tsx
@@ -10,6 +10,22 @@ const App: React.FC = () => {
   const [nbaPlayer, setNbaPlayer] = useState(nbaPlayers[0]);
   const [wnbaPlayer, setWnbaPlayer] = useState(wnbaPlayers[0]);
   const [matchPercentage, setMatchPercentage] = useState<number | null>(null);
+  const [helloMessage, setHelloMessage] = useState<string>('');
+  const [statusMessage, setStatusMessage] = useState<string>('');
+
+  const fetchHello = () => {
+    fetch("http://127.0.0.1:5000/hello")
+      .then(response => response.json())
+      .then(data => setHelloMessage(data.message))
+      .catch(error => console.error("Error fetching hello message:", error));
+  };
+
+  const fetchStatus = () => {
+    fetch("http://127.0.0.1:5000/status")
+      .then(response => response.json())
+      .then(data => setStatusMessage(data.status))
+      .catch(error => console.error("Error fetching status:", error));
+  };
 
   const handleMatch = () => {
     setMatchPercentage(getRandomMatchPercentage());
@@ -18,7 +34,19 @@ const App: React.FC = () => {
   return (
     <header className="App-header">
       <div className="App">
+          <div className="flex justify-center items-center h-screen">
+          <img 
+            src="https://i.scdn.co/image/ab67616d00001e025c421ed3721b09aade087582" 
+            onError={(e) => (e.currentTarget.src = "https://via.placeholder.com/150")} 
+            alt="LeBron James" 
+          />
+          </div>
           <h1>NBA & WNBA Matchmaker</h1>
+          <button onClick={fetchHello}>Fetch Hello</button>
+          <p>{helloMessage}</p>
+          <button onClick={fetchStatus}>Fetch Status</button>
+          <p>Status: {statusMessage}</p>
+          
           <div>
             <label>NBA Player: </label>
             <select value={nbaPlayer} onChange={(e) => setNbaPlayer(e.target.value)}>
@@ -41,7 +69,6 @@ const App: React.FC = () => {
           )}
       </div>
     </header>
-
   );
 };
 
